44b74772de05ac1d536dd31a6113a40c05e2b484
Yang Yang <persistence@vip.qq.com>
2025-07-05 15:08:07 +0800

    fixed the issue that no task found for user throw 422 error to frontend

--- server/src/api/tasks.ts
+++ server/src/api/tasks.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { taskService } from '../services/TaskService.js';
-import { CustomError } from '../classes/CustomError.js';
-import { ErrorCodes, TaskResDto, TaskUpdateReqDto, TaskCreateReqDto, ApiResponse } from '@fullstack/common';
+import { TaskResDto, TaskUpdateReqDto, TaskCreateReqDto, ApiResponse } from '@fullstack/common';
 import { mapObject } from '../utils/mappers.js';
 import { createApiResponse } from '../utils/apiUtils.js';
 
@@ -57,9 +56,6 @@ router.delete('/:taskId',
 router.get('/me', async (req, res, next) => {
     const userId = req.user!.userId;
     const tasks = await taskService.getTasksByUserId(userId);
-    if (tasks.length === 0) {
-        throw new CustomError("No tasks found for this user", ErrorCodes.NOT_FOUND);
-    }
     const data: TaskResDto[] = tasks.map(task => mapObject(task, new TaskResDto()));
     res.json(createApiResponse<TaskResDto[]>(data));
 });
