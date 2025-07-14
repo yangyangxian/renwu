8c8cf5d05299cc48d31533ac418443f042f42b6b
Yang Yang <persistence@vip.qq.com>
2025-07-14 22:57:51 +0800

    fixed the issue to trigger postbuild

--- server/src/utils/path.ts
+++ server/src/utils/path.ts
@@ -2,8 +2,7 @@ import path from 'path'
 import appConfig from '../appConfig';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-export const serverRootDir : string = path.resolve(__dirname, '..');
+const currentFilename = fileURLToPath(import.meta.url);
+const currentDirname = path.dirname(currentFilename);
 export const serverRootDir : string = path.resolve(currentDirname, '..');
 export const staticDistDir : string = path.resolve(serverRootDir, appConfig.staticDir);
