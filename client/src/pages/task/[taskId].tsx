7d16b2faf832040110fbd490b605a92994343e4c
Yang Yang <persistence@vip.qq.com>
2026-05-23 13:12:11 +0800

    feat: polish ui detail

--- client/src/pages/task/[taskId].tsx
+++ client/src/pages/task/[taskId].tsx
@@ -6,7 +6,7 @@ export default function TaskPage() {
   const { taskId } = useParams<{ taskId: string }>();
   if (!taskId) return <div className="p-8 text-center text-lg">Task not found.</div>;
   return (
-    <Card className="flex w-full p-6 m-3 rounded-xl">
+    <Card className="m-3 flex w-full rounded-xl p-6 shadow-none">
       <div className="w-full max-w-7xl">
         <TaskDetail taskId={taskId} />
       </div>
